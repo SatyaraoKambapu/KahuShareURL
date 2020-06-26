a40d4c475ce69937fb1ae6f24f5500adff91edb6
SatyaraoKambapu <satyapersonal1269@gmail.com>
2020-06-26 20:01:02 +0530

    Loading input params.

--- src/main/java/com/spireon/hackthon/kahuweb/controller/ShareLocationController.java
+++ src/main/java/com/spireon/hackthon/kahuweb/controller/ShareLocationController.java
@@ -1,16 +1,20 @@
 package com.spireon.hackthon.kahuweb.controller;
 
 import com.spireon.hackthon.kahuweb.service.TrackVehicleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -18,6 +22,7 @@ import java.util.Date;
 @Controller
 public class ShareLocationController {
 
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	TrackVehicleService service;
 
@@ -30,9 +35,15 @@ public class ShareLocationController {
 	}
 
 	@RequestMapping(value = "/track", method = RequestMethod.GET)
-	public String showTodos(ModelMap model) {
-		String name = getLoggedInUserName(model);
-		model.put("name", name);
+	public String showTodos(@RequestParam("id") String id, @RequestParam("requestId") String requestId) {
+		logger.info("Id=" + id + " and " + "requestId=" + requestId);
+		if(!StringUtils.isEmpty(id)) {
+			String[] vinsArray = id.split(",");
+			for (String vin: vinsArray) {
+				logger.info("VIN = " + vin);
+			}
+
+		}
 		return "track-map";
 	}
 
